7ac4ce1a59d4b1cef6511ef3d41d646a3b36c4ea
Codigo Aberto <marcio.marques@codigoaberto.pt>
2014-05-12 16:14:27 +0100

    Update default_status.php

--- tmpl/default_status.php
+++ tmpl/default_status.php
@@ -9,14 +9,13 @@
 **/
 
 defined( '_JEXEC' ) or die;
-
 if ( JCck::on() ) {
 	$class	=	'';
 	$href	=	'#';
 	if ( !$app->input->getBool( 'hidemainmenu' ) ) {
-		$layout	=	dirname(__FILE__) . '/modal_icon.php';
-		JCckDevIntegration::appendModal( $layout, 'collapseModal3', '.cck-quickadd > button', array( 'quickadd'=>1 ) );
-		$class	=	' btn-success';
+	$layout	=	dirname(__FILE__) . '/modal_icon.php';
+	JCckDevIntegration::appendModal( $layout, 'collapseModal3', '.cck-quickadd > button', array( 'quickadd'=>1 ) );
+//		$class	=	' btn-success';
 	}
 	?>
   <div class="casebmanager btn-toolbar">
@@ -24,13 +23,18 @@ if ( JCck::on() ) {
 				<p><?php echo JText::_('MOD_CASEBMANAGER_CONTENT_MANAGER_TOOLBAR');?></p>
 
 	</div>
-	<div class="btn-group cck-quickadd">
-		<button href="<?php echo $href; ?>" class="btn btn-small<?php echo $class; ?>" data-toggle="modal" data-target="#collapseModal3">
+	<div class="btn-group">
+    <?php if(!file_exists(JPATH_ROOT.'/templates/'.$app->getTemplate().'/less/uikit/uikit.less')) { ?>
+	<button href="<?php echo $href; ?>" class="btn btn-small<?php echo $class; ?>" data-toggle="modal" data-target="#collapseModal3">
 			<i class="icon-plus"></i>
 			<?php echo $label; ?>
 		</button>
+   <?php } else {  ?>
+       <button class="uk-button" data-uk-modal="{target:'#addcontent'}"><i class="icon-plus"></i>
+			<?php echo $label; ?></button>
+    <?php } ?>
 	</div>
-	<div class="btn-group">
+    <div class="btn-group">
 		<a href="<?php echo JURI::base();?>index.php/content-management" class="btn btn-small<?php echo $class; ?>">
 			<i class="icon-list-view"></i>
 			<?php echo JText::_('Content Management'); ?>
